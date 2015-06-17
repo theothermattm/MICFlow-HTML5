88e55536eeea3359d26be185d5346e8a06051037
Thomas Conner <thomas.conner@me.com>
2015-06-17 11:54:30 -0400

    Wrote a setup script to help a user configure the app
    Signed-off-by: Thomas Conner <thomas.conner@me.com>

---
+++ setup.js
@@ -0,0 +1,83 @@
+/*eslint-disable */
+
+'use strict';
+
+var prompt = require('prompt');
+var chalk = require('chalk');
+var fs = require('fs');
+var path = require('path');
+var configFile = path.join(__dirname, 'app/scripts/config.js');
+
+console.log(chalk.cyan('This utility will walk you through setting up MIC Flow for HTML5.'));
+console.log(chalk.cyan('Please refer to http://devcenter.kinvey.com/android/guides/mobile-identity-connect on how to setup MIC.'))
+console.log(chalk.cyan('Press ^C at any time to quite.'));
+console.log('');
+
+// Customize the prompt
+prompt.message = '';
+prompt.delimiter = '';
+
+// Start the prompt
+prompt.start();
+
+// Get appKey, appSecret, and redirectUri
+prompt.get({
+  properties: {
+    appKey: {
+      description: chalk.white('What is your app key?')
+    },
+    appSecret: {
+      description: chalk.white('What is your app secret?')
+    },
+    redirectUri: {
+      description: chalk.white('What is your redirect uri?')
+    }
+  }
+}, function(err, result) {
+  if (err) {
+    console.log(chalk.red(err));
+    return;
+  }
+
+  var configFileData = '\'use strict\';\n' +
+    '\n' +
+    'window.APP_KEY = \'' + result.appKey + '\';\n' +
+    'window.APP_SECRET = \'' + result.appSecret + '\';\n' +
+    'window.REDIRECT_URI = \'' + result.redirectUri + '\';';
+
+  console.log();
+  console.log('About to write to ' + configFile + '.');
+  console.log();
+  console.log(chalk.bgBlack(configFileData));
+  console.log();
+
+  // Check if it is correct
+  prompt.get({
+    properties: {
+      correct: {
+        description: chalk.white('Is this ok?'),
+        default: 'yes'
+      }
+    }
+  }, function(err, result) {
+    if (err) {
+      console.log(chalk.red(err));
+      return;
+    }
+
+    if (result.correct.toLowerCase() === 'yes' || result.correct.toLowerCase() === 'y') {
+      // Write config file
+      fs.writeFile(configFile, configFileData, function(err) {
+        if (err) {
+          console.log(chalk.red(err));
+          return;
+        }
+
+        console.log(chalk.white('Done.'));
+      });
+    }
+    else {
+      console.log(chalk.red('Aborted.'));
+    }
+  });
+});
